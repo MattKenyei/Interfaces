ebab6bd8eb93d0ad073193a3558b6bee8c367a63
MattKenyei <mattkenyei@gmail.com>
2024-03-12 00:48:55 +0300

    added 2lab(part 1)

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace interface_lab2
+{
+    partial class FormTryes
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTries = new System.Windows.Forms.Label();
+            this.labelWord = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTries
+            // 
+            this.labelTries.AutoSize = true;
+            this.labelTries.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTries.Location = new System.Drawing.Point(340, 80);
+            this.labelTries.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTries.Name = "labelTries";
+            this.labelTries.Size = new System.Drawing.Size(60, 29);
+            this.labelTries.TabIndex = 7;
+            this.labelTries.Text = "Try: ";
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWord.Location = new System.Drawing.Point(340, 137);
+            this.labelWord.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(83, 29);
+            this.labelWord.TabIndex = 8;
+            this.labelWord.Text = "Word: ";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.Green;
+            this.buttonStart.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.ForeColor = System.Drawing.Color.Yellow;
+            this.buttonStart.Location = new System.Drawing.Point(13, 46);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 49);
+            this.buttonStart.TabIndex = 18;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.Location = new System.Drawing.Point(340, 207);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(71, 29);
+            this.labelTime.TabIndex = 19;
+            this.labelTime.Text = "time: ";
+            this.labelTime.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(194, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 34);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(194, 46);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 34);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.Green;
+            this.button3.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(194, 94);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 34);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Green;
+            this.button4.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(194, 136);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 34);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "D";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.BackColor = System.Drawing.Color.Green;
+            this.button5.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(194, 178);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 34);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "E";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.BackColor = System.Drawing.Color.Green;
+            this.button6.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.ForeColor = System.Drawing.Color.Black;
+            this.button6.Location = new System.Drawing.Point(194, 220);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 34);
+            this.button6.TabIndex = 22;
+            this.button6.Text = "F";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.BackColor = System.Drawing.Color.Green;
+            this.button7.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.ForeColor = System.Drawing.Color.Black;
+            this.button7.Location = new System.Drawing.Point(194, 262);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 34);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "G";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.AutoSize = true;
+            this.button8.BackColor = System.Drawing.Color.Green;
+            this.button8.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(194, 304);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 34);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "H";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSize = true;
+            this.button9.BackColor = System.Drawing.Color.Green;
+            this.button9.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.ForeColor = System.Drawing.Color.Black;
+            this.button9.Location = new System.Drawing.Point(194, 346);
+            this.button9.Margin = new System.Windows.Forms.Padding(4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 34);
+            this.button9.TabIndex = 27;
+            this.button9.Text = "I";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // FormTryes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(736, 527);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelWord);
+            this.Controls.Add(this.labelTries);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormTryes";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormTryes_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelTries;
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Spire.Xls;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace interface_lab2
+{
+    public partial class FormTryes : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        Stopwatch stopwatch = new Stopwatch();
+        static int NumButtons = 9, NumTries = 5;
+        int NumVisible = 2, CurTry = 1;
+        bool IsStarted = false, IsFinished = false;
+        string CurWord = "";
+        static string WordString = "Word: ", TryBeginString = "Try: ", TryEndString = " / ", TimeString = "time: ";
+
+        SortedList<int, string> Words = new SortedList<int, string>()
+        {
+            {1, "A"},
+            {2, "B"},
+            {3, "C"},
+            {4, "D"},
+            {5, "E"},
+            {6, "F"},
+            {7, "G"},
+            {8, "H"},
+            {9, "I"}
+        };
+        List<Button> buttons = new List<Button>();
+
+        SortedList<int, float> MedTimes = new SortedList<int, float>();
+        List<float> times = new List<float>();
+
+        public FormTryes()
+        {
+            InitializeComponent();
+
+            buttons.Add(button1);
+            buttons.Add(button2);
+            buttons.Add(button3);
+            buttons.Add(button4);
+            buttons.Add(button5);
+            buttons.Add(button6);
+            buttons.Add(button7);
+            buttons.Add(button8);
+            buttons.Add(button9);
+        }
+
+        private void OutData()
+        {
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = workbook.Worksheets[0];
+
+            worksheet.Range[1, 1].Value = "n";
+            worksheet.Range[1, 2].Value = "t";
+
+            for (int i = 1; i <= MedTimes.Count(); i++)
+            {
+                worksheet.Range[i + 1, 2].Value = MedTimes.ElementAt(i - 1).Value.ToString();
+                worksheet.Range[i + 1, 1].Value = MedTimes.ElementAt(i - 1).Key.ToString();
+            }
+
+            workbook.SaveToFile("C:\\Users\\maksp\\OneDrive\\Рабочий стол\\interface lab2\\1.xlsx", ExcelVersion.Version2013);
+        }
+
+        private void RightClick()
+        {
+            stopwatch.Stop();
+            times.Add(((float)(stopwatch.Elapsed.Seconds + stopwatch.Elapsed.Milliseconds * 0.001)));
+
+            foreach (Button button in buttons) button.Visible = false;
+
+            if (CurTry < NumTries) CurTry++;
+            else {
+                MedTimes.Add(NumVisible, times.Sum()/NumTries);
+                times.Clear();
+                CurTry = 1;
+                NumVisible++;
+            };
+
+            buttonStart.Visible = true;
+
+            labelTime.Text = TimeString + stopwatch.Elapsed.Seconds.ToString() + "," + stopwatch.Elapsed.Milliseconds.ToString();
+            labelTime.Visible = true;
+
+            if (NumVisible > NumButtons) {
+                OutData();
+                this.Close(); 
+            };
+            IsStarted = false;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            List<int> vectorNum = new List<int>();
+
+            int num = random.Next(1, NumVisible + 1);
+            for (int i = 0; i < NumVisible; i++)
+            {
+                while (vectorNum.Contains(num)) num = random.Next(1, NumVisible + 1);
+                vectorNum.Add(num);
+            }
+
+            for (int i = 0; i < NumVisible; i++) {
+                string str = "";
+                Words.TryGetValue(vectorNum.ElementAt(i), out str);
+                buttons.ElementAt(i).Text = str;
+            }
+
+            buttonStart.Visible = false;
+
+            for (int i = 0; i < NumVisible; i++) buttons[i].Visible = true;
+
+            IsStarted = true;
+
+            Words.TryGetValue(random.Next(1, NumVisible+1), out CurWord);
+            labelWord.Text = WordString + CurWord;
+
+            labelTries.Text = TryBeginString + CurTry + TryEndString + NumTries;
+
+            Point buttonLocation = buttons[0].PointToScreen(Point.Empty);
+            buttonLocation.X = buttons[0].PointToScreen(Point.Empty).X + buttons[0].Size.Width + 10;
+            buttonLocation.Y = buttons[0].PointToScreen(Point.Empty).Y + (buttons[0].Size.Height + 5) * NumVisible / 2;
+            SetCursorPos(buttonLocation.X, buttonLocation.Y);
+
+            labelTime.Visible = false;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsStarted &&  CurWord == button1.Text) RightClick();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button2.Text) RightClick();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button3.Text) RightClick();
+        }
+
+        private void FormTryes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button4.Text) RightClick();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button5.Text) RightClick();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button6.Text) RightClick();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button7.Text) RightClick();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button8.Text) RightClick();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (IsStarted && CurWord == button9.Text) RightClick();
+        }
+    }
+}
